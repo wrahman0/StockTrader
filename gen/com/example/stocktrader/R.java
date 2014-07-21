07b844155b754787dc9bcb06e7fe94cd06c332bb
j449li <george.76105@gmail.com>
2014-07-20 20:41:35 -0400

    Issue#13
    Switching from FragmentTabHost to ViewPager to host tabs to allow
    swiping between tabs.

--- gen/com/example/stocktrader/R.java
+++ gen/com/example/stocktrader/R.java
@@ -8,6 +8,12 @@
 package com.example.stocktrader;
 
 public final class R {
+    public static final class array {
+        /**  Text Resources 
+  Tab Labels 
+         */
+        public static final int tab_labels=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class color {
@@ -76,44 +82,44 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int ImageView01=0x7f09000f;
-        public static final int TableRow01=0x7f09000e;
-        public static final int TextView06=0x7f090010;
-        public static final int cardStockName=0x7f09001f;
-        public static final int cardStockSymbol=0x7f090020;
-        public static final int cardTableRow=0x7f09001e;
-        public static final int detailsBuyButton=0x7f090004;
-        public static final int detailsChange=0x7f090009;
-        public static final int detailsDaysHigh=0x7f09000a;
-        public static final int detailsDaysLow=0x7f09000b;
-        public static final int detailsExchange=0x7f090003;
-        public static final int detailsLastTradePriceOnly=0x7f090008;
-        public static final int detailsName=0x7f090001;
-        public static final int detailsSymbol=0x7f090002;
-        public static final int detailsYearHigh=0x7f09000c;
-        public static final int detailsYearLow=0x7f09000d;
-        public static final int negativeButton=0x7f090019;
-        public static final int newsCardBody=0x7f090007;
-        public static final int newsCardImageView=0x7f090006;
-        public static final int newsLinearLayout=0x7f090012;
-        public static final int newsNotFound=0x7f090015;
-        public static final int newsScrollView=0x7f090011;
-        public static final int newsTableRow=0x7f090013;
-        public static final int positiveButton=0x7f09001a;
-        public static final int purchaseDialogQuantity=0x7f090017;
-        public static final int searchListView=0x7f09001d;
-        public static final int searchView=0x7f09001c;
-        public static final int signupStartingCash=0x7f090024;
-        public static final int signupUsername=0x7f090023;
-        public static final int stockCardLinearLayout=0x7f09001b;
-        public static final int stockDetailsButton=0x7f090021;
-        public static final int tabHost=0x7f090022;
-        public static final int tableRow1=0x7f090000;
-        public static final int tableRow2=0x7f090005;
-        public static final int tableRow3=0x7f090014;
-        public static final int textView1=0x7f090016;
-        public static final int totalPriceTextView=0x7f090018;
-        public static final int userSetupComplete=0x7f090025;
+        public static final int ImageView01=0x7f0a000f;
+        public static final int TableRow01=0x7f0a000e;
+        public static final int TextView06=0x7f0a0010;
+        public static final int cardStockName=0x7f0a001f;
+        public static final int cardStockSymbol=0x7f0a0020;
+        public static final int cardTableRow=0x7f0a001e;
+        public static final int detailsBuyButton=0x7f0a0004;
+        public static final int detailsChange=0x7f0a0009;
+        public static final int detailsDaysHigh=0x7f0a000a;
+        public static final int detailsDaysLow=0x7f0a000b;
+        public static final int detailsExchange=0x7f0a0003;
+        public static final int detailsLastTradePriceOnly=0x7f0a0008;
+        public static final int detailsName=0x7f0a0001;
+        public static final int detailsSymbol=0x7f0a0002;
+        public static final int detailsYearHigh=0x7f0a000c;
+        public static final int detailsYearLow=0x7f0a000d;
+        public static final int negativeButton=0x7f0a0019;
+        public static final int newsCardBody=0x7f0a0007;
+        public static final int newsCardImageView=0x7f0a0006;
+        public static final int newsLinearLayout=0x7f0a0012;
+        public static final int newsNotFound=0x7f0a0015;
+        public static final int newsScrollView=0x7f0a0011;
+        public static final int newsTableRow=0x7f0a0013;
+        public static final int pager=0x7f0a0022;
+        public static final int positiveButton=0x7f0a001a;
+        public static final int purchaseDialogQuantity=0x7f0a0017;
+        public static final int searchListView=0x7f0a001d;
+        public static final int searchView=0x7f0a001c;
+        public static final int signupStartingCash=0x7f0a0024;
+        public static final int signupUsername=0x7f0a0023;
+        public static final int stockCardLinearLayout=0x7f0a001b;
+        public static final int stockDetailsButton=0x7f0a0021;
+        public static final int tableRow1=0x7f0a0000;
+        public static final int tableRow2=0x7f0a0005;
+        public static final int tableRow3=0x7f0a0014;
+        public static final int textView1=0x7f0a0016;
+        public static final int totalPriceTextView=0x7f0a0018;
+        public static final int userSetupComplete=0x7f0a0025;
     }
     public static final class layout {
         public static final int details_redesigned=0x7f030000;
@@ -126,71 +132,70 @@ public final class R {
         public static final int user_signup=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        /**  Text Resources 
- General 
+        public static final int action_settings=0x7f070001;
+        /**  General 
          */
-        public static final int app_name=0x7f060000;
-        public static final int card_image_button_desc=0x7f06000e;
-        public static final int card_stock_change=0x7f06000d;
-        public static final int card_stock_exchange=0x7f06000b;
-        public static final int card_stock_last_trade_price_only=0x7f06000c;
+        public static final int app_name=0x7f070000;
+        public static final int card_image_button_desc=0x7f07000e;
+        public static final int card_stock_change=0x7f07000d;
+        public static final int card_stock_exchange=0x7f07000b;
+        public static final int card_stock_last_trade_price_only=0x7f07000c;
         /**  Card 
          */
-        public static final int card_stock_name=0x7f060009;
-        public static final int card_stock_symbol=0x7f06000a;
-        public static final int details_content=0x7f06001c;
-        public static final int details_news_URL=0x7f06001d;
-        public static final int details_published_date=0x7f06001f;
-        public static final int details_publisher=0x7f06001e;
-        public static final int details_stock_amount_to_buy=0x7f060022;
+        public static final int card_stock_name=0x7f070009;
+        public static final int card_stock_symbol=0x7f07000a;
+        public static final int details_content=0x7f07001c;
+        public static final int details_news_URL=0x7f07001d;
+        public static final int details_published_date=0x7f07001f;
+        public static final int details_publisher=0x7f07001e;
+        public static final int details_stock_amount_to_buy=0x7f070022;
         /**  Details Buy Section 
          */
-        public static final int details_stock_buy_button=0x7f060020;
-        public static final int details_stock_change=0x7f060016;
-        public static final int details_stock_days_high=0x7f060018;
-        public static final int details_stock_days_low=0x7f060017;
-        public static final int details_stock_exchange=0x7f060014;
-        public static final int details_stock_last_trade_price_only=0x7f060015;
-        public static final int details_stock_name=0x7f060012;
-        public static final int details_stock_symbol=0x7f060013;
-        public static final int details_stock_user_cash=0x7f060021;
-        public static final int details_stock_year_high=0x7f06001a;
-        public static final int details_stock_year_low=0x7f060019;
+        public static final int details_stock_buy_button=0x7f070020;
+        public static final int details_stock_change=0x7f070016;
+        public static final int details_stock_days_high=0x7f070018;
+        public static final int details_stock_days_low=0x7f070017;
+        public static final int details_stock_exchange=0x7f070014;
+        public static final int details_stock_last_trade_price_only=0x7f070015;
+        public static final int details_stock_name=0x7f070012;
+        public static final int details_stock_symbol=0x7f070013;
+        public static final int details_stock_user_cash=0x7f070021;
+        public static final int details_stock_year_high=0x7f07001a;
+        public static final int details_stock_year_low=0x7f070019;
         /**  Details Stock View News 
          */
-        public static final int details_title=0x7f06001b;
-        public static final int details_title_buy=0x7f060011;
-        public static final int details_title_stock_news=0x7f060010;
+        public static final int details_title=0x7f07001b;
+        public static final int details_title_buy=0x7f070011;
+        public static final int details_title_stock_news=0x7f070010;
         /**  Details Stock View Stock 
          */
-        public static final int details_title_stock_quote=0x7f06000f;
+        public static final int details_title_stock_quote=0x7f07000f;
         /**  Error Messages 
          */
-        public static final int empty_search_alert=0x7f060005;
-        public static final int invalid_search_alert=0x7f060006;
+        public static final int empty_search_alert=0x7f070005;
+        public static final int invalid_search_alert=0x7f070006;
         /**  Extra stuff 
          */
-        public static final int lorem_ipsum=0x7f060028;
-        public static final int news_not_found=0x7f060008;
-        public static final int purchase_dialog_negative_button=0x7f060025;
-        public static final int purchase_dialog_positive_button=0x7f060024;
-        public static final int purchase_dialog_stocks_price=0x7f060027;
-        public static final int purchase_dialog_stocks_text=0x7f060026;
+        public static final int lorem_ipsum=0x7f070028;
+        public static final int news_not_found=0x7f070008;
+        public static final int purchase_dialog_negative_button=0x7f070025;
+        public static final int purchase_dialog_positive_button=0x7f070024;
+        public static final int purchase_dialog_stocks_price=0x7f070027;
+        public static final int purchase_dialog_stocks_text=0x7f070026;
         /**  Purchase Dialog 
          */
-        public static final int purchase_dialog_title=0x7f060023;
-        public static final int signup_setup_complete=0x7f060004;
-        public static final int signup_starting_cash=0x7f060003;
+        public static final int purchase_dialog_title=0x7f070023;
+        public static final int signup_setup_complete=0x7f070004;
+        public static final int signup_starting_cash=0x7f070003;
         /**  User Signup page 
          */
-        public static final int signup_username=0x7f060002;
+        public static final int signup_username=0x7f070002;
         /**  SearchStockFragment 
          */
-        public static final int stock_search_bar_hint=0x7f060007;
+        public static final int stock_search_bar_hint=0x7f070007;
     }
     public static final class style {
         /** 
@@ -214,13 +219,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
         /**  Button 
          */
-        public static final int button_text=0x7f070002;
+        public static final int button_text=0x7f080002;
     }
 }
--- src/com/example/stocktrader/StockTraderActivity.java
+++ src/com/example/stocktrader/StockTraderActivity.java
@@ -1,43 +1,107 @@
 package com.example.stocktrader;
 
+import java.util.ArrayList;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 
 public class StockTraderActivity extends FragmentActivity {
 	public static final String TAG = "StockTraderActivity";
-	
-	private FragmentTabHost mFragmentTabHost;
-	
+
+	private ArrayList<String>mTabLabels = new ArrayList<String>();
+	private ArrayList<Fragment>mTabFragments = new ArrayList<Fragment>();
+
+	private ViewPager mViewPager;
+	private TabFragmentPagerAdapter mPagerAdapter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-		
+
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
 		setContentView(R.layout.stock_trader_main);
-		
-		mFragmentTabHost = (FragmentTabHost)findViewById(R.id.tabHost);
-        mFragmentTabHost.setup(this, getSupportFragmentManager(),R.layout.stock_trader_main);
-        
-        mFragmentTabHost.addTab(mFragmentTabHost.newTabSpec("search").setIndicator("Search"),
-        		SearchStockFragment.class, null);
-        
-        //temporary placeholder tabs
-        mFragmentTabHost.addTab(mFragmentTabHost.newTabSpec("my stocks").setIndicator("My Stocks"),
-        		SearchStockFragment.class, null);
-        mFragmentTabHost.addTab(mFragmentTabHost.newTabSpec("account").setIndicator("Account"),
-        		SearchStockFragment.class, null);
-//        mFragmentTabHost.addTab(mFragmentTabHost.newTabSpec("search").setIndicator("Search"),
-//                null, null);
-//        mFragmentTabHost.addTab(mFragmentTabHost.newTabSpec("account").setIndicator("Account"),
-//                null, null);
+
+		mViewPager = (ViewPager)findViewById(R.id.pager);
+
+		mPagerAdapter = new TabFragmentPagerAdapter(getSupportFragmentManager());
+		mViewPager.setAdapter(mPagerAdapter);
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position) {
+				// When swiping between pages, select the
+				// corresponding tab.
+				getActionBar().setSelectedNavigationItem(position);
+			}
+		});
+
+		final ActionBar actionBar = getActionBar();
+		// Specify that tabs should be displayed in the action bar.
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Create a tab listener that is called when the user changes tabs.
+		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+
+			@Override
+			public void onTabSelected(Tab tab, FragmentTransaction ft) {
+				mViewPager.setCurrentItem(tab.getPosition());
+			}
+
+			@Override
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+
+		//get string array for tab labels
+		Resources res = getResources();
+		String[] tabLabels = res.getStringArray(R.array.tab_labels);
+
+		for(String tabLabel:tabLabels){
+			actionBar.addTab(actionBar.newTab()
+					.setText(tabLabel)
+					.setTabListener(tabListener));
+		}
 
 	}
-	
+
+	public class TabFragmentPagerAdapter extends FragmentStatePagerAdapter {
+		public TabFragmentPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int i) {
+			Fragment fragment = new SearchStockFragment();
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			return 3;
+		}
+
+	}
+
 }
